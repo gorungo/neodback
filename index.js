b65bc2529f01394b05e1e36d34c731f2c5e55769
inkas <denis.g.petrov@gmail.com>
2022-12-29 13:47:18 +1000

    add test

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ const PORT = 3132;
 const app = express()
 app.use(express.json())
 
-app.get('/api/test/finish', (req,res) => {
-  sendMail().then(result => {
+app.post('/api/test/finish',  (req,res) => {
+  sendMail(req.body).then(result => {
     res.status(200).json(result)
   })
 })
 
 app.get('/api/test/test', (req,res) => {
   res.status(200).json('ok')
+})
+
+app.post('/api/test/body', (req,res) => {
+  res.status(200).json(req.body)
 })
 
 app.listen(PORT, () => console.log('Started on: ' + PORT))
